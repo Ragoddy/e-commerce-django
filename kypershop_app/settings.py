b0195b30d4ec1942b0acab1f3de5d5db6ad56056
Freddy Gomez <freddyalbertogomez.r@gmail.com>
2020-04-03 21:40:15 -0500

    add static root parameter in settings

--- kypershop_app/settings.py
+++ kypershop_app/settings.py
@@ -133,8 +133,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+#static files
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 #media files
 

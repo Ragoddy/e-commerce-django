f7e7303243e7bb14578eb9902f807d57e2883cad
Freddy Gomez <freddyalbertogomez.r@gmail.com>
2020-04-06 23:58:21 -0500

    edit templates and add create kyper from landing page

--- landing/urls.py
+++ landing/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.landing_page, name='landing_page'),
+    path('', views.LandingKyperView.as_view(), name='landing_page'),
+    path('create_kyper/', views.LandingCreateKyperView.as_view(), name='create_kyper_landing'),
 ]
--- landing/views.py
+++ landing/views.py
@@ -1,4 +1,38 @@
 from django.shortcuts import render
+from django.views.generic import View, ListView, FormView
+from django.http import HttpResponseRedirect, HttpResponse
 
-def landing_page(request):
-    return render(request, 'landing/index.html', {})
+from api.serializers.serializer_markets import MarketSerializer, TelephoneSerializer
+
+class LandingKyperView(View):
+    def get(self, request, *args, **kwargs):
+        context = {}
+        return render(request, 'landing/index.html', context)
+
+class LandingCreateKyperView(View):
+    def post(self, request, *args, **kwargs):
+        
+        data = {
+                'name': request.POST['name'],
+                'addresses': request.POST['addresses'],
+                'city': request.POST['city'],
+                'minimun_price': 4000,
+                'state': 0
+                }
+        
+        serializer = MarketSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            
+            data_phone = {
+                          'type_telephone': 2,
+                          'number': request.POST['phone'], 
+                          'first': 1, 
+                          'market': serializer.data['pk']
+                        }
+            
+            serializer = TelephoneSerializer(data=data_phone)
+            if serializer.is_valid():
+                serializer.save()
+                
+        return HttpResponseRedirect("/")
